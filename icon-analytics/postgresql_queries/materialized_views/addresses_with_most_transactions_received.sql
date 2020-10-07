a1713c474310d9f2e2047392668a5b0e2137e511
Kevin Patel <kevinbhailu@gmail.com>
2020-10-07 11:27:43 -0400

    Update addresses_with_most_transactions_received.sql

--- icon-analytics/postgresql_queries/materialized_views/addresses_with_most_transactions_received.sql
+++ icon-analytics/postgresql_queries/materialized_views/addresses_with_most_transactions_received.sql
@@ -1,6 +1,6 @@
-create materialized view addresses_with_most_transactions_received as
-select transactions_count.ct, transactions_count.to_address
+CREATE materialized view addresses_with_most_transactions_received AS
+SELECT transactions_count.ct, transactions_count.to_address
 FROM(
-    (SELECT count(*) as ct, to_address
-    FROM transactions group by to_address)
+    (SELECT COUNT(*) AS ct, to_address
+    FROM transactions GROUP BY to_address)
 ) transactions_count;
