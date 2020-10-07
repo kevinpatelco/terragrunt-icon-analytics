397d3522ba13835043a6918d7b46cae87fcd2776
Kevin Patel <kevinbhailu@gmail.com>
2020-10-07 11:32:45 -0400

    Update smart_contract_addresses_with_most_received_transactions.sql

--- icon-analytics/postgresql_queries/materialized_views/smart_contract_addresses_with_most_received_transactions.sql
+++ icon-analytics/postgresql_queries/materialized_views/smart_contract_addresses_with_most_received_transactions.sql
@@ -3,7 +3,7 @@ SELECT temp_table.address, temp_table.name, temp_table.ct
 FROM(
     SELECT s.address, s.name, count(*) AS ct from
     score_addresses AS s
-    join
+    JOIN
     reduced_trans AS r
     ON s.address = r.to_address
     GROUP BY s.address, s.name
