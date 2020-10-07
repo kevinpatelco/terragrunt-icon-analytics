e212ffd7539c6f1b4a244c0834897f6f2bd5e63f
Kevin Patel <kevinbhailu@gmail.com>
2020-10-07 11:32:23 -0400

    Create smart_contract_addresses_with_most_received_transactions.sql

---
+++ icon-analytics/postgresql_queries/materialized_views/smart_contract_addresses_with_most_received_transactions.sql
@@ -0,0 +1,10 @@
+CREATE materialized view score_addresses_with_most_transactions AS
+SELECT temp_table.address, temp_table.name, temp_table.ct
+FROM(
+    SELECT s.address, s.name, count(*) AS ct from
+    score_addresses AS s
+    join
+    reduced_trans AS r
+    ON s.address = r.to_address
+    GROUP BY s.address, s.name
+) temp_table;
